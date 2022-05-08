2fa1d9a47a7898053b8ac7b2d5b519eca0d72e15
yingchen123542 <54613135+yingchen123541@users.noreply.github.com>
2022-05-08 12:43:27 -0700

    created cart manager view to manage deleting and adding stuffs from shopping cart

--- finalProject/Shared/Models/CartManager.swift
+++ finalProject/Shared/Models/CartManager.swift
@@ -5,4 +5,19 @@
 //  Created by yingying Chen on 5/8/22.
 //
 
-import
+import Foundation
+
+class CartManager: ObservableObject {
+    @Published private(set) var products: [Product] = []
+    @Published private(set) var total: Int = 0
+    
+    func addToCart(product: Product) {
+        products.append(product)
+        total += product.price
+    }
+    
+    func removeFromCart(product: Product){
+        products = products.filter { $0.id != product.id }
+        total -= product.price
+    }
+}
